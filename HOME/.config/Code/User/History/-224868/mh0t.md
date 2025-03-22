cfc398cd02152573ebd909038da7708db640c0d1
Bolsic <vladanbasic@gmail.com>
2025-03-22 17:24:55 +0100

    Stop tracking HOME/.config/Code/ files so they are ignore

--- HOME/.config/Code/User/History/-224868/mh0t.md
+++
@@ -1,3 +0,0 @@
-## Core rules
-
-pg.8 "If this attack kills the enemy and no others you, you may remain hidden instead. "
--- HOME/.config/Code/User/History/47053143/Hheg.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -g fish_greeting ""
-end
-
-# Define an alias for dotfiles
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
